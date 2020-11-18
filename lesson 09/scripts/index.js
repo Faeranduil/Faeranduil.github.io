6db86223ce4807870a6f243e79d6bf608a9c080e
Faeranduil <rit19001@byui.edu>
2020-11-18 14:37:17 -0700

    make for loop

--- lesson 09/scripts/index.js
+++ lesson 09/scripts/index.js
@@ -11,104 +11,36 @@ fetch(requestURL)
 
     })
     .then(function (jsonObject) {
-            console.log(jsonObject);
-            console.table(jsonObject);
-            const towns = jsonObject['towns'];
-            let town = document.createElement('section');
-            let h2 = document.createElement('h2');
-            let h3 = document.createElement('p');
-            let p2 = document.createElement('p');
-            let p3 = document.createElement('p');
-            let p4 = document.createElement('p');
-            let image = document.createElement('img');
+        console.log(jsonObject);
+        console.table(jsonObject);
+        const towns = jsonObject['towns'];
+        for (let i = 0; i < towns.length; i++) {
+            if (towns[i].name == "Preston" || towns[i].name == "Fish Haven" || towns[i].name == "Soda Springs") {
+                let town = document.createElement('section');
+                let h2 = document.createElement('h2');
+                let h3 = document.createElement('p');
+                let p2 = document.createElement('p');
+                let p3 = document.createElement('p');
+                let p4 = document.createElement('p');
+                let image = document.createElement('img');
 
-            h2.textContent = towns[5]["name"];
-            h3.textContent = towns[5]["motto"];
-            p2.textContent = "Year Founded: " + towns[5]["yearFounded"];
-            p3.textContent = "Population: " + towns[5]["currentPopulation"];
-            p4.textContent = "Annual Rainfall: " + towns[5]["averageRainfall"];
-            image.textContent = towns[5]["photo"];
+                h2.textContent = towns[i]["name"];
+                h3.textContent = towns[i]["motto"];
+                p2.textContent = "Year Founded: " + towns[i]["yearFounded"];
+                p3.textContent = "Population: " + towns[i]["currentPopulation"];
+                p4.textContent = "Annual Rainfall: " + towns[i]["averageRainfall"];
+                image.textContent = towns[i]["photo"];
 
-            town.appendChild(h2);
-            town.appendChild(h3);
-            town.appendChild(p2);
-            town.appendChild(p3);
-            town.appendChild(p4);
-            town.appendChild(image);
+                town.appendChild(h2);
+                town.appendChild(h3);
+                town.appendChild(p2);
+                town.appendChild(p3);
+                town.appendChild(p4);
+                town.appendChild(image);
 
-            image.setAttribute('src', towns[5]["photo"]);
+                image.setAttribute('src', towns[5]["photo"]);
 
-            document.querySelector('.preston').appendChild(town); 
+                document.querySelector('.townie').appendChild(town);
+            }
+        }
     });
-
-    fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    
-    })
-    .then(function (jsonObject) {
-            console.table(jsonObject);
-            const towns = jsonObject['towns'];
-            let town = document.createElement('section');
-            let h2 = document.createElement('h2');
-            let h3 = document.createElement('p');
-            let p2 = document.createElement('p');
-            let p3 = document.createElement('p');
-            let p4 = document.createElement('p');
-            let image = document.createElement('img');
-
-            h2.textContent = towns[6]["name"];
-            h3.textContent = towns[6]["motto"];
-            p2.textContent = "Year Founded: " + towns[6]["yearFounded"];
-            p3.textContent = "Population: " + towns[6]["currentPopulation"];
-            p4.textContent = "Annual Rainfall: " + towns[6]["averageRainfall"];
-            image.textContent = towns[6]["photo"];
-
-            town.appendChild(h2);
-            town.appendChild(h3);
-            town.appendChild(p2);
-            town.appendChild(p3);
-            town.appendChild(p4);
-            town.appendChild(image);
-
-            image.setAttribute('src', towns[6]["photo"]);
-
-            document.querySelector('.sodaSprings').appendChild(town);
-        }); 
-
-    fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    
-    })
-
-    .then(function (jsonObject) {
-            console.table(jsonObject);
-            const towns = jsonObject['towns'];
-            let town = document.createElement('section');
-            let h2 = document.createElement('h2');
-            let h3 = document.createElement('p');
-            let p2 = document.createElement('p');
-            let p3 = document.createElement('p');
-            let p4 = document.createElement('p');
-            let image = document.createElement('img');
-
-            h2.textContent = towns[1]["name"];
-            h3.textContent = towns[1]["motto"];
-            p2.textContent = "Year Founded: " + towns[1]["yearFounded"];
-            p3.textContent = "Population: " + towns[1]["currentPopulation"];
-            p4.textContent = "Annual Rainfall: " + towns[1]["averageRainfall"];
-            image.textContent = towns[1]["photo"];
-
-            town.appendChild(h2);
-            town.appendChild(h3);
-            town.appendChild(p2);
-            town.appendChild(p3);
-            town.appendChild(p4);
-            town.appendChild(image);
-
-            image.setAttribute('src', towns[1]["photo"]);
-
-            document.querySelector('.fishHaven').appendChild(town);
-        });
-    
