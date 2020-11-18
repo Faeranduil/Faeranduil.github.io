7402c69954a3d00f8d332f98e5e15301bf72db4f
Faeranduil <rit19001@byui.edu>
2020-11-18 14:07:46 -0700

    changes

--- lesson 09/scripts/index.js
+++ lesson 09/scripts/index.js
@@ -22,12 +22,12 @@ fetch(requestURL)
             let p4 = document.createElement('p');
             let image = document.createElement('img');
 
-            h2.textContent = towns[4]["name"];
-            h3.textContent = towns[4]["motto"];
-            p2.textContent = "Year Founded: " + towns[4]["yearFounded"];
-            p3.textContent = "Population: " + towns[4]["currentPopulation"];
-            p4.textContent = "Annual Rainfall: " + towns[4]["averageRainfall"];
-            image.textContent = towns[4]["photo"];
+            h2.textContent = towns[5]["name"];
+            h3.textContent = towns[5]["motto"];
+            p2.textContent = "Year Founded: " + towns[5]["yearFounded"];
+            p3.textContent = "Population: " + towns[5]["currentPopulation"];
+            p4.textContent = "Annual Rainfall: " + towns[5]["averageRainfall"];
+            image.textContent = towns[5]["photo"];
 
             town.appendChild(h2);
             town.appendChild(h3);
@@ -36,7 +36,7 @@ fetch(requestURL)
             town.appendChild(p4);
             town.appendChild(image);
 
-            image.setAttribute('src', towns[4]["photo"]);
+            image.setAttribute('src', towns[5]["photo"]);
 
             document.querySelector('.preston').appendChild(town); 
     });
@@ -57,12 +57,12 @@ fetch(requestURL)
             let p4 = document.createElement('p');
             let image = document.createElement('img');
 
-            h2.textContent = towns[5]["name"];
-            h3.textContent = towns[5]["motto"];
-            p2.textContent = "Year Founded: " + towns[5]["yearFounded"];
-            p3.textContent = "Population: " + towns[5]["currentPopulation"];
-            p4.textContent = "Annual Rainfall: " + towns[5]["averageRainfall"];
-            image.textContent = towns[5]["photo"];
+            h2.textContent = towns[6]["name"];
+            h3.textContent = towns[6]["motto"];
+            p2.textContent = "Year Founded: " + towns[6]["yearFounded"];
+            p3.textContent = "Population: " + towns[6]["currentPopulation"];
+            p4.textContent = "Annual Rainfall: " + towns[6]["averageRainfall"];
+            image.textContent = towns[6]["photo"];
 
             town.appendChild(h2);
             town.appendChild(h3);
@@ -71,7 +71,7 @@ fetch(requestURL)
             town.appendChild(p4);
             town.appendChild(image);
 
-            image.setAttribute('src', towns[5]["photo"]);
+            image.setAttribute('src', towns[6]["photo"]);
 
             document.querySelector('.sodaSprings').appendChild(town);
         }); 
